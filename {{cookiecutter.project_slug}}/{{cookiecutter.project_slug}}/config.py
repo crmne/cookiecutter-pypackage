e4fc592bd640ff3b767e483a3a0a9ed1a44912ef
Carmine Paolino <carmine@paolino.me>
2015-12-09 14:07:10 +0100

    updated gitignore and added config

---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from yconf import BaseConfiguration
+
+
+class Config(BaseConfiguration):
+    def __init__(self, **kwargs):
+        # Overridden initializer that sets a default config path
+        super(Config, self).__init__(**kwargs)
+        self.configPath = "config.yaml"
