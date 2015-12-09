1be3667cad8721069538330e783eac7d28e7b421
Carmine Paolino <carmine@paolino.me>
2015-12-09 14:40:57 +0100

    pep8

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/config.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/config.py
@@ -3,6 +3,7 @@ from yconf import BaseConfiguration
 
 
 class Config(BaseConfiguration):
+
     def __init__(self, **kwargs):
         # Overridden initializer that sets a default config path
         super(Config, self).__init__(**kwargs)
