4d484846acc0c9c4e678fd5706d068b75d5042a7
Carmine Paolino <carmine@paolino.me>
2015-12-09 14:42:43 +0100

    clearer output in post gen project

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,12 +3,12 @@
 # fail on error
 set -e
 
-echo "Setting up local git repository..."
+echo ">>> Setting up local git repository..."
 git init
 git add .
 pre-commit install
 git commit -a -m "Initial Cookiecutter Commit."
 git checkout -b "{{ cookiecutter.first_feature_branch_name }}"
-echo "Setting up virtualenv..."
+echo ">>> Setting up virtualenv..."
 virtualenv --system-site-packages env
-echo "Project {{ cookiecutter.project_slug }} initialized successfully."
+echo ">>> Project {{ cookiecutter.project_slug }} initialized successfully."
