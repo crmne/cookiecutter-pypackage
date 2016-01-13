9ff40058426ccbb247d1d3398b77d101085184b5
Carmine Paolino <carmine@paolino.me>
2016-01-13 13:37:51 +0100

    unified yaml extension

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/config.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/config.py
@@ -7,4 +7,4 @@ class Config(BaseConfiguration):
     def __init__(self, **kwargs):
         # Overridden initializer that sets a default config path
         super(Config, self).__init__(**kwargs)
-        self.configPath = "config.yaml"
+        self.configPath = "config.yml"
